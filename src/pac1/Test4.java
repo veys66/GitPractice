16f27ab5c9932263fdd9ef9c5200db9ca580a4fe
veys66 <veysdoganer@gmail.com>
2022-12-10 19:14:14 +0300

    added Test1.java file

---
+++ src/pac1/Test4.java
@@ -0,0 +1,16 @@
+package pac1;
+
+public class Test4 {
+
+    public Test4() {
+    }
+
+    @Override
+    public String toString() {
+        return "Test4{}";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
